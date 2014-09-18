9db013a9ca9283ed88d20f330e29be984e5ceb47
pratikbhatia199 <pratik.bhatia.199@gmail.com>
2014-09-17 23:35:49 -0400

    Added Stack, Queue, EntrySet traversal

--- src/CollectionsExample.java
+++ src/CollectionsExample.java
@@ -1,6 +1,15 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.LinkedList;
+import java.util.Stack;
 public class CollectionsExample {
 	public static void main(String args[])throws IOException{
 		int x[]={1, 3, 4, 5, 6, 7};
@@ -17,6 +26,57 @@ public class CollectionsExample {
 		while(it.hasNext()){
 			System.out.println(it.next());
 		}
+		System.out.println("Reverse Sorting");
+		Collections.sort(arr_list, Collections.reverseOrder());
+		Iterator rit = arr_list.iterator();
+		while(rit.hasNext()){
+			System.out.println(rit.next());
+		}
+		HashMap<Integer, ArrayList<Integer>> hm = new HashMap<Integer,ArrayList<Integer>>();
+		for (int key:x){
+			hm.put(key, arr_list);			
+		}
+		
+		Set key_set = hm.keySet();
+		Iterator sit = key_set.iterator();
+		int total_sum = 0;
+		while(sit.hasNext()){
+			int value = (int)sit.next();
+			System.out.println(value);
+			total_sum = total_sum + value;
+			System.out.println("Total sum:"+total_sum);	
+			System.out.println(hm.get(value));
+		}
+		
+		for(Map.Entry<Integer, ArrayList<Integer>> entry : hm.entrySet()){
+			Integer key = entry.getKey();
+			ArrayList<Integer> value = entry.getValue();
+			System.out.println("Key: "+key+" Value: "+value);			
+		}
+		
+		TreeMap<Integer, ArrayList<Integer>> tm = new TreeMap<Integer, ArrayList<Integer>>(Collections.reverseOrder());
+		for(int value: arr_list){
+			tm.put(value, arr_list);
+		}
+		for(Map.Entry<Integer, ArrayList<Integer>> entry:tm.entrySet()){
+			int key = entry.getKey();
+			ArrayList<Integer> val_arr_list = entry.getValue();
+			System.out.println("Key: "+key+" value: "+val_arr_list);			
+		}
+		LinkedList<Integer> linked_list_queue = new LinkedList();
+		for(int value: arr_list){
+			linked_list_queue.addLast(value);
+		}
+		System.out.println(linked_list_queue);
+		Iterator lit = linked_list_queue.iterator();
+		while(lit.hasNext()){
+			System.out.println(lit.next());
+		}
+		Stack<Integer> s = new Stack<Integer>();		
+		for(int value: arr_list){
+			s.push(value);
+		}
+		System.out.println(s);
 		
 	}
 
